dce332fdba6a1799cffcae4bc30f123bf2506f8e
akshat11x <120719341+akshat11x@users.noreply.github.com>
2024-07-07 22:56:29 +0530

    Add files via upload

---
+++ js/js1.js
@@ -0,0 +1,32 @@
+console.log("Hi")
+let a=[1,2,3,4,5]
+
+console.log(a.shift())
+console.log(a.shift())
+console.log(a)
+a.unshift(10)
+console.log(a)
+
+function muFunc(){
+    console.log("Anni love bhabhi")
+    return 1;
+}
+
+let b={
+    "Case_1" : "Prazzu Anni ka katega",
+    "Case_2" : "prazzu Ko dahej mai fortuner milegi"
+}
+
+console.log(b.Case_1)
+console.log(b["Case_2"])
+
+// console.log(muFunc())
+console.log(parseInt("5",6))
+
+let i = 10;
+while(i>0)
+{
+    console.log(` Hello Prazul ${5*i}`);
+    i--;
+}
+
